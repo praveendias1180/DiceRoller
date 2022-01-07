503dd9d4d34c1db8ac42219e06651f4d64845ca0
Praveen Dias <praveendias1180@gmail.com>
2022-01-07 23:24:33 +0530

    Attaching to a button.

--- app/src/main/java/range/quest/diceroller/MainActivity.kt
+++ app/src/main/java/range/quest/diceroller/MainActivity.kt
@@ -2,10 +2,14 @@ package range.quest.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import range.quest.diceroller.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+        val rollButton: Button = findViewById(id.roll_button)
+        rollButton.text = getString(string.button_text_new)
     }
 }
