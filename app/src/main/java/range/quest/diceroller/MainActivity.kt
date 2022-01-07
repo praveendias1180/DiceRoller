2bfdc89628ebe46f44815fa877ab9981d0f10552
Praveen Dias <praveendias1180@gmail.com>
2022-01-07 23:27:32 +0530

    Display toast message on click.

--- app/src/main/java/range/quest/diceroller/MainActivity.kt
+++ app/src/main/java/range/quest/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package range.quest.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import range.quest.diceroller.R.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(layout.activity_main)
         val rollButton: Button = findViewById(id.roll_button)
         rollButton.text = getString(string.button_text_new)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
